5b77398649e88f90faf65cff0a4f164446cf86b1
daniellecroxton <danikrist@gmail.com>
2017-10-25 22:23:59 -0600

    Updated Controllers with basic routes

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,35 @@
 class MoviesController < ApplicationController
+
+#Movies Index
+  get '/movies' do
+    erb :'movies/movies'
+  end
+
+#New Movie
+  get '/movies/new' do
+    erb : 'movies/create_movie'
+  end
+
+  post '/movies' do
+
+  end
+
+#Edit Movie
+  get '/movies/:id/edit' do
+    erb :'movies/edit_movie'
+  end
+
+  patch '/movies/:id' do
+
+  end
+
+#Show Movie and Delete
+  get 'movies/:id' do
+    erb: 'movies/show_movie'
+  end
+
+  delete 'movies/:id/delete' do
+
+  end
+
 end
