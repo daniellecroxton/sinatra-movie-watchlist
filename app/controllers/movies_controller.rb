4085e1284317cfa75b93ece21f551e5fbb57c808
daniellecroxton <danikrist@gmail.com>
2017-10-27 15:40:01 -0600

    Added functionality in new movie post route to prevent duplicate movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -20,13 +20,18 @@ class MoviesController < ApplicationController
   end
 
   post '/movies' do
+    binding.pry
     if params[:title] == ""
       redirect to "/movies/new"
-    else
+    elsif current_user.movies.find_by(title: params[:title]) == nil
       @movie = Movie.new(params)
       @movie.user_id = current_user.id
       @movie.save
       flash[:message] = "Successfully created movie."
+      redirect "/movies/#{@movie.slug}"
+    else
+      @movie = current_user.movies.find_by(title: params[:title])
+      flash[:message] = "Movie already exists, click 'Edit Movie' to change your movie."
       redirect "/movies/#{@movie.slug}"
     end
   end
@@ -43,7 +48,14 @@ class MoviesController < ApplicationController
     end
 
     delete '/movies/:slug/delete' do
-
+      @movie = Movie.find_by_slug(params[:slug])
+      # binding.pry
+      if logged_in? && @movie.user_id == current_user.id
+        @movie.delete
+        redirect '/movies'
+      else
+        redirect '/login'
+      end
     end
 
 #Edit Movie
